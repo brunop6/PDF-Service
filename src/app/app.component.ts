2409afce8f17c65d70044d8e2db6f863061a51b8
brunop6 <peresbruno6@gmail.com>
2023-02-28 22:44:59 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { PdfService } from './services/pdf.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testePDF';
+  
+  constructor(private pdfService: PdfService) { }
+
+  ngOnInit(): void {
+    this.pdfService.createPdf();
+  }
 }
